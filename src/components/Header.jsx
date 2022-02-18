4fec166aabe15630984c1a860ebe27927f8516a7
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-02-18 14:48:36 -0300

    ajustando Lint do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-import { Component } from "react";
+import React, { Component } from 'react';
 
 class Header extends Component {
-    render() {
-        return <header><h1>Sistema Solar</h1></header>
-    }
+  render() {
+    return (
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
 }
 
 export default Header;
